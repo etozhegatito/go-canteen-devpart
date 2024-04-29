22f95c01a3886b53a689d9e6867a810cfa9582b5
Akarys <prof.aka777@gmail.com>
2024-04-30 00:04:36 +0600

    Database

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"go-canteen-devpart/auth"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"log"
-	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/register", auth.RegisterHandler)
-	http.HandleFunc("/login", auth.LoginHandler)
-
-	log.Println("Starting server on :8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	//http.HandleFunc("/register", auth.RegisterHandler)
+	//http.HandleFunc("/login", auth.LoginHandler)
+	//
+	//log.Println("Starting server on :8080...")
+	//if err := http.ListenAndServe(":8080", nil); err != nil {
+	//	log.Fatal("ListenAndServe: ", err)
+	//}
+	dbs := "host=localhost user=postgres dbname=postgres password=mysecretpassword port=5432"
+	_, err := gorm.Open(postgres.Open(dbs), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database", err)
 	}
 }
