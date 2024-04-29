d4c4ee7157374e56c7acd154cc9cccd5a9f25ce9
Akarys <prof.aka777@gmail.com>
2024-04-30 00:56:36 +0600

    User model done

--- db/model.go
+++ db/model.go
@@ -1,1 +1,17 @@
 package db
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type User struct {
+	gorm.Model            // Включает поля ID, CreatedAt, UpdatedAt, DeletedAt
+	Name        string    `gorm:"not null"`
+	Surname     string    `gorm:"not null"`
+	Email       string    `gorm:"uniqueIndex;not null"`
+	Password    string    `gorm:"not null"`
+	Phone       string    `gorm:"not null"`
+	DateOfBirth time.Time `gorm:"type:date"`
+	IsAdmin     bool      `gorm:"default:false"`
+}
